7c32c74810ee51ec2197b518622684d28e14b59b
root <root@k9296v.add.bjdt.qihoo.net>
2018-02-22 11:41:21 +0800

    Update ci-test.sh

--- ci-test.sh
+++ ci-test.sh
@@ -2,7 +2,7 @@
 echo '>>> Get old container id'
 CID=$(docker ps | grep "github-test" | awk '{print $1}')
 echo $CID
-sudo /bin/docker build -t github-test /home/kangkai-iri/git/Jenkins_Docker_Ansible | tee /home/kangkai-iri/git/Jenkins_Docker_Ansible/Docker_build_result.log
+sudo /bin/docker build -t github-test /home/kangkai-iri/git/Jenkins_Docker_2048 | tee /home/kangkai-iri/git/Jenkins_Docker_2048/Docker_build_result.log
 echo '>>> Stopping old container'
 if [ "$CID" != "" ];then
 sudo /bin/docker stop $CID
@@ -11,4 +11,4 @@ fi
 echo '>>> Starting new container'
 sudo /bin/docker run -p 3000:80 -d github-test
 IP=$(hostname -i)
-echo '>>> Game is running at http://'$IP':3000'
+echo '>>> The game is running at http://'$IP':3000'
